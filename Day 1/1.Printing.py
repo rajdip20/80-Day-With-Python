5c43ada8e20145c26aafac128bea2ee478f0c5c2
rajdip20 <dasrajdip78754282@gmail.com>
2022-01-02 18:39:25 +0530

    Initial commit

---
+++ Day 1/1.Printing.py
@@ -0,0 +1,13 @@
+'''
+Example Output
+After you have written your code, you should run your program and it should print the following:
+
+Day 1 - Python Print Function
+The function is declared like this:
+print('what to print')
+
+'''
+
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
