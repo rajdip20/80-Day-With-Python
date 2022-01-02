578b770e0669225674379424c1ac0ed16e9d73bd
rajdip20 <dasrajdip78754282@gmail.com>
2022-01-02 18:41:03 +0530

    Initial commit

---
+++ Day 1/2.Debugging Practice.py
@@ -0,0 +1,15 @@
+'''
+Example Output
+When you run your program, it should print the following:
+
+Day 1 - String Manipulation
+String Concatenation is done with the "+" sign.
+e.g. print("Hello " + "world")
+New lines can be created with a backslash and n.
+
+'''
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
