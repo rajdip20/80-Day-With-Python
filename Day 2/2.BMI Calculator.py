122934174c96f53049b2ccb95ca7a63db61596c1
rajdip20 <dasrajdip78754282@gmail.com>
2022-01-03 15:09:23 +0530

    Initial commit

---
+++ Day 2/2.BMI Calculator.py
@@ -0,0 +1,13 @@
+
+'''
+Write a program that calculates the Body Mass Index (BMI) from a user's weight and height.
+'''
+
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+print(int(float(weight)/(float(height)*float(height))))
