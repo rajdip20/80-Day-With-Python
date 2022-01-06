05f2e6c01ca1a53a964a4bfd92971a0cd752ddf1
rajdip20 <dasrajdip78754282@gmail.com>
2022-01-06 07:25:46 +0530

    Initial commit

---
+++ Day 5/1.Average Height.py
@@ -0,0 +1,34 @@
+'''
+You are going to write a program that calculates the average student height from a List of heights.
+
+e.g. student_heights = [180, 124, 165, 173, 189, 169, 146]
+
+The average height can be calculated by adding all the heights together and dividing by the total number of heights.
+
+e.g.
+
+180 + 124 + 165 + 173 + 189 + 169 + 146 = 1146
+
+There are a total of 7 heights in student_heights
+
+1146 ÷ 7 = 163.71428571428572
+
+Average height rounded to the nearest whole number = 164
+'''
+
+# 🚨 Don't change the code below 👇
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+    student_heights[n] = int(student_heights[n])
+# 🚨 Don't change the code above 👆
+
+
+#Write your code below this row 👇
+
+add = 0
+length = len(student_heights)
+for student_height in student_heights:
+    add = add + student_height
+
+result = round(add/length)
+print(result)
