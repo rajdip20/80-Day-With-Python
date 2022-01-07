de765c75ab6ec84345db1b76657f5593dc38bbcb
rajdip20 <dasrajdip78754282@gmail.com>
2022-01-07 06:46:46 +0530

    Initial commit

---
+++ Day 6/1.Hurdle 1.py
@@ -0,0 +1,27 @@
+
+'''
+https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+
+This is a challenge to solve Hurdle 1 problem.
+'''
+
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+
+for i in range(0, 6):
+    jump()
