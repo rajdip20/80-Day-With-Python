020a9c47dbd1d42d73fa58556c61f2d3c4a1d8f2
rajdip20 <dasrajdip78754282@gmail.com>
2022-01-07 07:00:41 +0530

    Initial commit

---
+++ Day 6/4.Hurdle 4.py
@@ -0,0 +1,30 @@
+
+'''
+https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%204&url=worlds%2Ftutorial_en%2Fhurdle4.json
+
+This is a challenge to solve Hurdle 4 problem.
+'''
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+def jump():
+    turn_left()
+    while wall_on_right():
+        move()
+    turn_right()
+    move()
+    turn_right()
+    while front_is_clear():
+        move()
+    turn_left()
+
+
+while at_goal() != True:
+    if wall_in_front():
+        jump()
+    else:
+        move()
