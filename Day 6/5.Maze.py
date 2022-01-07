75ef0233aaef6572f18846a48c4701563bce1db3
rajdip20 <dasrajdip78754282@gmail.com>
2022-01-07 07:01:18 +0530

    Initial commit

---
+++ Day 6/5.Maze.py
@@ -0,0 +1,25 @@
+
+'''
+https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+
+This is a challenge to solve Maze problem.
+'''
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+while front_is_clear():
+    move()
+turn_left()
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
