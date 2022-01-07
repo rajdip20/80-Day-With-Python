a45b6c81c78147cdc91bc646e353b94cf37a55fd
rajdip20 <dasrajdip78754282@gmail.com>
2022-01-07 06:53:56 +0530

    Initial commit

---
+++ Day 6/3.Hurdle 3.py
@@ -0,0 +1,28 @@
+
+'''
+https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%204&url=worlds%2Ftutorial_en%2Fhurdle3.json
+
+This is a challenge to solve Hurdle 3 problem.
+'''
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+def jump():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+
+while at_goal() != True:
+    if front_is_clear():
+        move()
+    else:
+        jump()
