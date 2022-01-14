c6362d3084838bebbbcd275b70ec2be615b35639
rajdip20 <dasrajdip78754282@gmail.com>
2022-01-14 15:07:37 +0530

    Initial commit

---
+++ Day 13/2.Debugging Odd or Even.py
@@ -0,0 +1,27 @@
+'''
+Instructions:
+    Read this the code in main.py
+    Spot the problems 🐞.
+    Modify the code to fix the program.
+
+Fix the code so that it works and passes the tests when you submit.
+'''
+
+
+'''
+number = int(input("Which number do you want to check?"))
+
+if number % 2 = 0:
+    print("This is an even number.")
+else:
+    print("This is an odd number.")
+
+'''
+#################################### Modified Program ####################################
+
+number = int(input("Which number do you want to check?"))
+
+if number % 2 == 0:
+    print("This is an even number.")
+else:
+    print("This is an odd number.")
