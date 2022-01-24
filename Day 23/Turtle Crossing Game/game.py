c2dcd9cfdcb8abdf08abac40e16ea4f0c32c20b5
rajdip20 <dasrajdip78754282@gmail.com>
2022-01-24 14:55:17 +0530

    Initial commit

---
+++ Day 23/Turtle Crossing Game/game.py
@@ -0,0 +1,41 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.title("Crossing Game")
+screen.tracer(0)
+
+player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(player.go_up, "Up")
+# screen.onkey(player.go_down, "Down")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    # Detect collision with car
+    for car in car_manager.all_cars:
+        if car.distance(player) < 25:
+            game_is_on = False
+            scoreboard.game_over()
+
+    # Detect a successful crossing
+    if player.is_finish():
+        player.go_to_start()
+        car_manager.level_up()
+        scoreboard.increase_level()
+
+
+screen.exitonclick()
