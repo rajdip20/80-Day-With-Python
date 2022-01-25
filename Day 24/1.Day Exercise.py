1a54bd1340e865ee3d5444812536cace317b4061
rajdip20 <dasrajdip78754282@gmail.com>
2022-01-25 11:39:30 +0530

    Initial commit

---
+++ Day 24/1.Day Exercise.py
@@ -0,0 +1,13 @@
+# with open("my_file.txt") as file:
+#     contents = file.read()
+#     print(contents)
+
+
+# with open("my_file.txt", mode="a") as file:
+#     file.write("\nNew text.")
+
+# with open("new_file.txt", mode="w") as file:
+#     file.write("New text.")
+
+# with open("C:/Users/user/Desktop/new_file.txt", mode="w") as file:
+#     file.write("New text.")
