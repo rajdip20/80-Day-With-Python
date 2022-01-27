2efb22a34553139c5e838a4720455db2ad9e4d98
rajdip20 <dasrajdip78754282@gmail.com>
2022-01-27 13:20:23 +0530

    Initial commit

---
+++ Day 26/5.Dictionary Comprehension 2.py
@@ -0,0 +1,15 @@
+# You are going to use Dictionary Comprehension to create a dictionary called result that takes each word
+# in the given sentence and calculates the number of letters in each word.
+
+# Try Googling to find out how to convert a sentence into a list of words.
+
+# Do NOT Create a dictionary directly. Try to use Dictionary Comprehension instead of a Loop.
+
+sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+# Don't change code above 👆
+
+# Write your code below:
+
+result = {count: len(count) for count in sentence.split()}
+
+print(result)
