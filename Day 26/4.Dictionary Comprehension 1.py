899bc3b2e22a917d0e24faf3449188c010cad55e
rajdip20 <dasrajdip78754282@gmail.com>
2022-01-27 13:20:12 +0530

    Initial commit

---
+++ Day 26/4.Dictionary Comprehension 1.py
@@ -0,0 +1,17 @@
+# 💪 This exercise is HARD
+
+# Take a look inside file1.txt and file2.txt. They each contain a bunch of numbers, each number on a new line.
+
+# You are going to create a list called result which contains the numbers that are common in both files.
+
+with open("file1.txt") as file1:
+    file_1_data = file1.readlines()
+
+with open("file2.txt") as file2:
+    file_2_data = file2.readlines()
+
+result = [int(num) for num in file_1_data if num in file_2_data]
+
+# Write your code above 👆
+
+print(result)
