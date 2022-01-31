e42d20689e745178d482c76c5f2337b36e3423af
rajdip20 <dasrajdip78754282@gmail.com>
2022-01-31 14:44:10 +0530

    Initial commit

---
+++ Day 30/2.IndexError Handling.py
@@ -0,0 +1,29 @@
+# fruits = ["Apple", "Pear", "Orange"]
+#
+# # Catch the exception and make sure the code runs without crashing.
+#
+#
+# def make_pie(index):
+#     fruit = fruits[index]
+#     print(fruit + " pie")
+#
+#
+# make_pie(4)
+
+#######################################################################
+
+fruits = ["Apple", "Pear", "Orange"]
+
+# Catch the exception and make sure the code runs without crashing.
+
+
+def make_pie(index):
+    try:
+        fruit = fruits[index]
+    except IndexError:
+        print("Fruit pie")
+    else:
+        print(fruit + " pie")
+
+
+make_pie(4)
