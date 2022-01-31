23250bde4af3d9e623bad8e11791cb1c851fdf50
rajdip20 <dasrajdip78754282@gmail.com>
2022-01-31 14:43:53 +0530

    Initial commit

---
+++ Day 30/1.Day Exercise.py
@@ -0,0 +1,46 @@
+# FileNotFoundError
+# with open("a_file.txt") as file:
+#     file.read()
+
+################################################################
+
+# try:
+#     file = open("a_file.txt")
+#     a_dictionary = {"key": "value"}
+#     # print(a_dictionary["non_existent_key"])
+#     print(a_dictionary["key"])
+# except FileNotFoundError:
+#     file = open("a_file.txt", "w")
+#     file.write("Something")
+# except KeyError as error_message:
+#     print(f"That key {error_message} does not exist.")
+# else:
+#     content = file.read()
+#     print(content)
+# finally:
+#     raise TypeError("This is an error that I made up.")
+
+##############################################################
+
+# KeyError
+# a_dictionary = {"key": "value"}
+# value = a_dictionary["non_existent_key"]
+
+# IndexError
+# fruit_list = ["Apple", "Banana", "Pear"]
+# fruit = fruit_list[3]
+
+# TypeError
+# text = "abc"
+# print(text + 5)
+
+####################################################################
+
+height = float(input("Height: "))
+weight = int(input("Weight: "))
+
+if height > 3:
+    raise ValueError("Human Height should not be over 3 meters.")
+
+bmi = weight / height ** 2
+print(bmi)
