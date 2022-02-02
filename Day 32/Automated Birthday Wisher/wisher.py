c01ef152f32af67f9def1434df9ca52a8c95d01b
rajdip20 <dasrajdip78754282@gmail.com>
2022-02-02 13:55:10 +0530

    Initial commit

---
+++ Day 32/Automated Birthday Wisher/wisher.py
@@ -0,0 +1,28 @@
+import smtplib
+import datetime as dt
+import pandas
+import random
+
+MY_EMAIL = "abc@gmail.com"    # Enter Your Mail
+MY_PASSWORD = "*******"       # Enter Your Password
+
+now = dt.datetime.now()
+today_tuple = (now.month, now.day)
+data = pandas.read_csv("birthdays.csv")
+birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+
+if today_tuple in birthdays_dict:
+    birthday_person = birthdays_dict[today_tuple]
+    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]", birthday_person["name"])
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=birthday_person["email"],
+            msg=f"Subject:Happy Birthday!\n\n{contents}"
+        )
