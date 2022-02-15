f7c86fe7650dd616d6c08b3ef41bd7ec53c45327
rajdip20 <dasrajdip78754282@gmail.com>
2022-02-15 17:13:38 +0530

    Initial commit

---
+++ Day 45/100 movies to watch/main.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
+
+# Write your code below this line 👇
+
+response = requests.get(URL)
+website_html = response.text
+
+soup = BeautifulSoup(website_html, "html.parser")
+
+all_movies = soup.find_all(name="h3", class_="title")
+
+movie_titles = [movie.getText() for movie in all_movies]
+movies = movie_titles[::-1]
+
+with open("movies.txt", mode="w") as file:
+    for movie in movies:
+        file.write(f"{movie}\n")
